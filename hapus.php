7fe00b7131f77fefe9b50f5db7b6176008f6e213
Daffa <daffaraihanzaki@gmail.com>
2018-10-30 15:46:48 +0700

    hapus

--- hapus.php
+++ hapus.php
@@ -1,6 +1,6 @@
 <?php
   include ("config.php");
-  $id = $_GET['Id'];
+  $id = $_GET['id'];
   $qry = "DELETE FROM users WHERE Id='$id'";
   mysqli_query($db, $qry);
   header("location:index.php");
